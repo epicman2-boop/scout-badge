89e3a34062d212236828feb0ce8aa4a6bbc70ffc
Joseph Converse <jpc6459@psu.edu>
2023-04-27 12:40:39 -0400

    v2

---
+++ scout-badge/src/badge-sticker.js
@@ -0,0 +1,80 @@
+import { LitElement, html, css } from 'lit-element';
+
+
+class BadgeSticker extends LitElement {
+
+  static get properties() {
+    return {
+      title: { type: String },
+      date: { type: String },
+      skills: { type: Array },
+      icon: { type: String },
+      locked: { type: Boolean },
+      verificationLink: { type: String },
+    };
+  }
+
+  static get styles() {
+    return css`
+      :host {
+        display: inline-block;
+        text-align: center;
+        position: relative;
+      }
+      .badge {
+        position: relative;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 200px;
+        height: 200px;
+        background-color: var(--badge-color, #000000);
+        border: 2px dashed var(--badge-stitch-color, #FFF);
+        border-radius: 50%;
+        box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.5);
+        overflow: hidden;
+        cursor: pointer;
+      }
+      .badge.locked {
+        opacity: 0.5;
+        cursor: not-allowed;
+      }
+      .badge-icon {
+        font-size: 70px;
+        color: var(--badge-icon-color, #ffffff);
+      }
+      .badge-label {
+        font-size: 18px;
+        font-weight: bold;
+        margin-top: 10px;
+        color: var(--badge-label-color, #ffffff);
+        text-align: center
+        width: 120px;
+      }
+      .badge-date {
+        font-size: 12px;
+        margin-top: 5px;
+        color: var(--badge-date-color, #ffffff);
+      }
+    `;
+  }
+
+  constructor() {
+    super();
+    this.locked = true;
+  }
+
+  render() {
+    return html`
+      <div class="badge ${this.locked ? 'locked' : ''}">
+        <div class="badge-icon">${this.icon}
+          <div class="badge-label">${this.title}</div>
+          <div class="badge-date">${this.date}</div>
+        </div>
+      </div>
+    `;
+  }
+
+}
+
+customElements.define('badge-sticker', BadgeSticker);
---
+++ scout-badge/src/merit-badge.js
@@ -0,0 +1,72 @@
+import { LitElement, html, css } from 'lit-element';
+import './badge-sticker.js';
+
+class MeritBadge extends LitElement {
+
+  static get properties() {
+    return {
+      title: { type: String },
+      date: { type: String },
+      skills: { type: Array },
+      icon: { type: String },
+      verificationLink: { type: String }
+    };
+  }
+
+  static get styles() {
+    return css`
+      :host {
+        display: block;
+        text-align: center;
+        position: relative;
+        margin-bottom: 50px;
+      }
+      button {
+        display: block;
+        margin: 0 auto;
+        padding: 10px 20px;
+        border: none;
+        background-color: var(--button-bg-color, #2196F3);
+        color: var(--button-text-color, #FFF);
+        border-radius: 5px;
+        font-size: 16px;
+        cursor: pointer;
+      }
+      button:active {
+        background-color: #0c375a;
+      }
+    `;
+  }
+
+  constructor() {
+    super();
+    this.locked = true;
+  }
+
+  render() {
+    return html`
+    <!-- badge -->
+      <badge-sticker 
+        .title=${this.title}
+        .date=${this.date}
+        .skills=${this.skills}
+        .icon=${this.icon}
+        .locked=${this.locked}
+        .verificationLink=${this.verificationLink}>
+      </badge-sticker>
+    <!-- button -->
+      <button id="unlockBadgeButton" 
+      @click="${this.handleButtonClick}"
+      >Unlock Badge</button>
+    `;
+  }
+
+  handleButtonClick() {
+    this.locked = false;
+    const today = new Date();
+    this.date = "Unlocked on " + today.toDateString();
+  }
+
+}
+
+customElements.define('merit-badge', MeritBadge);
